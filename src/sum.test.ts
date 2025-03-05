56f21b92a5bc28d4c50f41701c30193ecc09b394
César Daniel dos Santos Rodrigues <proxxima-cesar@franca.intranet>
2025-03-05 17:30:45 -0300

    beforeAll

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,10 +1,18 @@
 import { sum } from "./server"
 
+
+
 describe("sum's", () => {
+
+  let sumResult: number
+
+  beforeAll(()=>{
+    sumResult = 10
+  })
   test("sum of 3 + 7 must be 10", () => {
     const result = sum(3, 7)
 
-    expect(result).toBe(10)
+    expect(result).toBe(sumResult)
   })
   test("sum of 4+4  must be 8", () => {
     const result = sum(4, 4)
