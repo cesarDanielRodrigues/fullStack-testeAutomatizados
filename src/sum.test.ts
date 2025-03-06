7f9dfd1be8f1d78b77031d1aa33291413649bda4
César Daniel dos Santos Rodrigues <proxxima-cesar@franca.intranet>
2025-03-06 09:44:21 -0300

    afterAll

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -9,11 +9,16 @@ describe("sum's", () => {
   beforeAll(()=>{
     sumResult = 10
   })
+  afterAll(()=>{
+    sumResult = 10
+  })
+
   test("sum of 3 + 7 must be 10", () => {
     const result = sum(3, 7)
 
     expect(result).toBe(sumResult)
   })
+
   test("sum of 4+4  must be 8", () => {
     const result = sum(4, 4)
 
